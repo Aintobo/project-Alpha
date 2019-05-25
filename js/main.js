ae97efab581a9480e6c1b4d5e0c638cad1859c49
Aintobo <50731971+Aintobo@users.noreply.github.com>
2019-05-25 09:05:02 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+function search(){
+    var view = document.getElementById('search_view');
+    if(view.style.display == 'block'){
+        view.style.display = 'none'
+    }
+    else{
+        view.style.display = 'block'
+    }
+
+    
+}
+function search_close(){
+    var view = document.getElementById('search_view');
+    view.style.display = 'none'
+}
+
